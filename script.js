f42a99ee6461f920d03b7f5b7797432d5797aa5a
mohd-sameer-576 <xmohdsameerx576@gmail.com>
2025-05-28 01:51:49 +0530

    making it more responsive

--- script.js
+++ script.js
@@ -1,39 +1,3 @@
-// document.getElementById('idCardForm').addEventListener('submit', function(e) {
-//     e.preventDefault();
-
-//     const collegename = document.getElementById('collegename').value.trim();
-//     const name = document.getElementById('name').value.trim();
-//     const rollno = document.getElementById('rollno').value.trim();
-//     const dob = document.getElementById('dob').value;
-//     const phone = document.getElementById('phone').value.trim();
-
-//     if (!name || !rollno || !dob || !phone) {
-//         alert("Please fill in all fields.");
-//         return;
-//     }
-
-//     const idCardHTML = `
-//     <div class="card">
-//         <div class="card-header">
-//             <p><strong>${collegename.toUpperCase()}</strong></p>
-//         </div>
-//         <div class="card-body">
-//             <div class="body">
-//             <p><strong>Name:</strong> ${name}</p>
-//             <p><strong>Roll No:</strong> ${rollno}</p>
-//             <p><strong>Date of Birth:</strong> ${dob}</p>
-//             <p><strong>Phone:</strong> ${phone}</p>
-//             </div>
-//             <div class="img-container">
-//             <img src="sameer.jpeg" alt="Profile Picture" class="profile-pic">
-//             </div>
-//         </div>
-//     </div>
-// `;
-
-
-//     document.getElementById('idCardPreview').innerHTML = idCardHTML;
-// });
 
 document.getElementById('idCardForm').addEventListener('submit', function (e) {
     e.preventDefault();
@@ -62,7 +26,7 @@ document.getElementById('idCardForm').addEventListener('submit', function (e) {
                 <div class="body">
                     <p><strong>Name:</strong> ${name}</p>
                     <p><strong>Roll No:</strong> ${rollno}</p>
-                    <p><strong>Date of Birth:</strong> ${dob}</p>
+                    <p><strong>DOB:</strong> ${dob}</p>
                     <p><strong>Phone:</strong> ${phone}</p>
                 </div>
                 <div class="img-container">
